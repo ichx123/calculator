27bd385f074644af19226eb34cbdd6e42350346c
ichx123 <ichx123@icloud.com>
2024-12-06 09:06:29 +0100

    feat: add function to operate the calculator
    The function operate() will take two numbers and a operator as arguments
    and return the result of the calculation based on which operator is used.

--- javascript.js
+++ javascript.js
@@ -20,3 +20,25 @@ function divide(num1, num2) {
         return num1/num2;
     }
 }
+
+ //Basic variables for using the calculator
+ let num1;
+ let num2;
+ let operator;
+
+//Function to operate the cakculator with the given input
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            return add(num1,num2);
+        case "-": 
+            return substract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            console.log("Error, you used no valid operator.")
+    }
+
+}
